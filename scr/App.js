47babde795f11403db38ff12d8f04b399e7f98c7
Brighel70 <bulgari.andrea@gmail.com>
2025-07-25 10:52:26 +0200

    Create App.js

---
+++ scr/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyDivinos from './components/MyDivinos';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyDivinos />
+    </div>
+  );
+}
+
+export default App;
